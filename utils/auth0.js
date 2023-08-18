d05408199c324b8de2bfd4bc7b49f5c1f19b024c
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-18 14:05:33 +0300

    Design inspo

--- utils/auth0.js
+++ utils/auth0.js
@@ -1,14 +1,16 @@
 import { initAuth0 } from '@auth0/nextjs-auth0';
 
 export default initAuth0({
-  domain: process.env.AUTH0_DOMAIN,
-  clientId: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  scope: 'openid profile',
-  redirectUri: process.env.AUTH0_REDIRECT_URI,
-  postLogoutRedirectUri: process.env.AUTH0_POST_LOGOUT_REDIRECT_URI,
-  session: {
-    cookieSecret: process.env.AUTH0_COOKIE_SECRET,
-    storeAccessToken: true
-  }
-});
+    domain: process.env.AUTH0_DOMAIN,
+    clientId: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    scope: process.env.AUTH0_SCOPE || 'openid profile',
+    redirectUri: process.env.REDIRECT_URI || 'https://crossfit-efesop.vercel.app/api/auth/callback',
+    postLogoutRedirectUri: process.env.POST_LOGOUT_REDIRECT_URI || 'https://crossfit-efesop.vercel.app/',
+    session: {
+      // The secret used to encrypt the cookie.
+      cookieSecret: process.env.AUTH0_SESSION_COOKIE_SECRET,
+      storeAccessToken: process.env.AUTH0_STORE_ACCESS_TOKEN || false
+    }
+  });
+  
