64e53f92b74f14fef36cf73b5c03560b71423fff
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-18 13:25:07 +0300

    Design inspo

--- pages/profile.js
+++ pages/profile.js
@@ -1,5 +1,6 @@
 import { useUser } from '@auth0/nextjs-auth0';
 
+
 export default function Profile() {
     const { user, isLoading } = useUser();
 
--- utils/auth0.js
+++ utils/auth0.js
@@ -5,8 +5,8 @@ export default initAuth0({
   clientId: '1ihqi1KdvUMwUXFtVvia5NBYdIyED5cu',
   clientSecret: 'UAMoiKsdfYT9KS7uTsI8VuAiCMo0ChT1vHKKXmDPG2fIDROTpcSGa-FmdR-Hkx8x',
   scope: 'openid profile',
-  redirectUri: 'http://localhost:3000/api/callback',
-  postLogoutRedirectUri: 'http://localhost:3000/',
+  redirectUri: 'https://crossfit-efesop.vercel.app',
+  postLogoutRedirectUri: 'https://crossfit-efesop.vercel.app',
   session: {
     cookieSecret: 'A_LONG_RANDOM_STRING',
     storeAccessToken: true
