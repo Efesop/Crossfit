767cb53c9fe01fbfe9b3044f91e9cc8ad847856a
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-18 14:55:42 +0300

    Design inspo

--- pages/api/auth/callback.js
+++ pages/api/auth/callback.js
@@ -1,10 +1,3 @@
-import auth0 from '../../../utils/auth0';
+import { handleCallback } from '@auth0/nextjs-auth0';
 
-export default async function callback(req, res) {
-  try {
-    await auth0.handleCallback(req, res, { redirectTo: '/' });
-  } catch (error) {
-    console.error(error);
-    res.status(error.status || 500).end(error.message);
-  }
-}
+export default handleCallback();
--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,10 +1,3 @@
-import auth0 from '../../../utils/auth0';
+import { handleLogin } from '@auth0/nextjs-auth0';
 
-export default async function login(req, res) {
-  try {
-    await auth0.handleLogin(req, res);
-  } catch (error) {
-    console.error(error);
-    res.status(error.status || 500).end(error.message);
-  }
-}
+export default handleLogin();
