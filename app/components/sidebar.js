0559d33247e81d4eeb72b128e668096ea1a0b4a0
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-19 13:49:13 +0300

    Design inspo working and semi functional

--- app/components/sidebar.js
+++ app/components/sidebar.js
@@ -1,4 +1,4 @@
-import { useUser } from '@auth0/nextjs-auth0/client/use-user';
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Sidebar() {
     const { user } = useUser();
