49da73b5e0dda475f9a968183a0aabda884adf74
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-19 13:44:06 +0300

    Design inspo working and semi functional

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 import '../app/globals.css';
 import RootLayout from '../app/layout';
 
