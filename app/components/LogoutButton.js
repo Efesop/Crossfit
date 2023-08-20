33ec8ce084355b452d03d95c01536611cdf3ddde
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-20 23:44:44 +0300

    Design inspo working and semi functional

--- app/components/LogoutButton.js
+++ app/components/LogoutButton.js
@@ -1,12 +1,12 @@
 import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function LogoutButton() {
-    const { user, logout } = useUser();
+    const { user } = useUser();
 
     if (!user) return null;
 
     const handleLogout = () => {
-        logout({ returnTo: window.location.origin });
+        window.location.href = "https:dev-to536ufgipngcm31.auth0.com/v2/logout?returnTo=https%3A%2F%2Fcrossfit-efesop.vercel.app%2F";
     };
 
     return (
