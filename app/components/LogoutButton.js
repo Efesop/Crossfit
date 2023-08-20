6544c456fa7802d7a1f217e99262212ebc412885
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-20 21:23:45 +0300

    Design inspo working and semi functional

--- app/components/LogoutButton.js
+++ app/components/LogoutButton.js
@@ -1,15 +1,23 @@
 import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function LogoutButton() {
-    const { user, logout, getIdTokenClaims } = useUser();
+    const { user, getIdTokenClaims } = useUser();
 
     if (!user) return null;
 
     const handleLogout = async () => {
         const tokenClaims = await getIdTokenClaims();
         const idToken = tokenClaims.__raw;
-        logout({ 
-            returnTo: window.location.origin,
+
+        // Initialize Auth0 WebAuth
+        const webAuth = new auth0.WebAuth({
+            domain: process.env.AUTH0_DOMAIN,
+            clientID: process.env.AUTH0_CLIENT_ID
+        });
+
+        // Use the logout method from WebAuth
+        webAuth.logout({
+            post_logout_redirect_uri: process.env.POST_LOGOUT_REDIRECT_URI,
             id_token_hint: idToken
         });
     };
