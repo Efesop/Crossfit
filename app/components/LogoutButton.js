a5c95901246b98199b307ecc4bd0fbc1489a8f62
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-20 21:13:05 +0300

    Design inspo working and semi functional

--- app/components/LogoutButton.js
+++ app/components/LogoutButton.js
@@ -1,19 +1,22 @@
-// components/LogoutButton.js
-// import { useUser } from '@auth0/nextjs-auth0';
 import { useUser } from '@auth0/nextjs-auth0/client';
 
-
 export default function LogoutButton() {
-    const { user, logout = () => console.error("Logout function is not defined") } = useUser();
+    const { user, logout, getIdTokenClaims } = useUser();
 
-  if (!user) return null;
+    if (!user) return null;
 
-  return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
-  Logout
-</button>
+    const handleLogout = async () => {
+        const tokenClaims = await getIdTokenClaims();
+        const idToken = tokenClaims.__raw;
+        logout({ 
+            returnTo: window.location.origin,
+            id_token_hint: idToken
+        });
+    };
 
-  );
+    return (
+        <button onClick={handleLogout}>
+            Logout
+        </button>
+    );
 }
-
-
