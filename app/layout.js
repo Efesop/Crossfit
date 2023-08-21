0769fa9b4862081681878d39ab1a20a6ff62d776
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-22 00:15:24 +0300

    Design inspo working authentication working

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { useUser } from '@auth0/nextjs-auth0/client';
 import { Fragment, useState } from 'react';
 import { Dialog, Menu, Transition } from '@headlessui/react';
 import LogoutButton from './components/LogoutButton';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
 import { useRouter } from 'next/router';
 import {
   Bars3Icon,
@@ -135,6 +137,47 @@ export default function Layout({ children }) {
                 <span className="sr-only">View notifications</span>
                 <BellIcon className="h-6 w-6" aria-hidden="true" />
               </button>
+              {/* Profile dropdown */}
+<Menu as="div" className="relative">
+  <Menu.Button className="-m-1.5 flex items-center p-1.5">
+    <span className="sr-only">Open user menu</span>
+    <img
+      className="h-8 w-8 rounded-full bg-gray-50"
+      src={user?.picture}
+      alt={user?.name}
+    />
+    <span className="hidden lg:flex lg:items-center">
+      <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
+        {user?.name}
+      </span>
+      <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+    </span>
+  </Menu.Button>
+  <Transition
+    as={Fragment}
+    enter="transition ease-out duration-100"
+    enterFrom="transform opacity-0 scale-95"
+    enterTo="transform opacity-100 scale-100"
+    leave="transition ease-in duration-75"
+    leaveFrom="transform opacity-100 scale-100"
+    leaveTo="transform opacity-0 scale-95"
+  >
+    <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+      {/* You can add more user navigation items here */}
+      <Menu.Item>
+        <a href="/profile" className="block px-3 py-1 text-sm leading-6 text-gray-900">
+          Your profile
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="/api/auth/logout" className="block px-3 py-1 text-sm leading-6 text-gray-900">
+          Sign out
+        </a>
+      </Menu.Item>
+    </Menu.Items>
+  </Transition>
+</Menu>
+
             </div>
           </div>
         </div>
