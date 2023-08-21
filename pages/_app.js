ac8c5da2d011bf70e84868485fee8d471064a5f1
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-21 20:37:08 +0300

    Design inspo working authentication working

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { UserProvider } from '@auth0/nextjs-auth0/client';
 import '../app/globals.css';
 import RootLayout from '../app/layout';
 
+
 export default function MyApp({ Component, pageProps }) {
     return (
       <UserProvider>
