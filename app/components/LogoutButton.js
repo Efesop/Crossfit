c92361cdea2e0b5ece8d17dbd7a5629ca8be3310
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-21 17:38:47 +0300

    Design inspo working and semi functional

--- app/components/LogoutButton.js
+++ app/components/LogoutButton.js
@@ -1,15 +1,19 @@
-import { useAuth0 } from '@auth0/nextjs-auth0/client';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
-export default function LogoutButton({ logoutDestination }) {
-  const { logout } = useAuth0();
+export default function LogoutButton({ logoutDestination = '/api/auth/logout' }) {
+  const { user } = useUser();
 
   const handleLogout = () => {
     try {
-      logout({ returnTo: logoutDestination });
+      // Use the provided logoutDestination or default to '/api/auth/logout'
+      window.location.href = logoutDestination;
     } catch (error) {
       console.error("Error during logout:", error);
     }
   };
+
+  // If there's no user, don't render the button
+  if (!user) return null;
 
   return (
     <button onClick={handleLogout}>
