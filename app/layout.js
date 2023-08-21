02666c280d7dd248f0470d3a5dca2787b6f328cc
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-21 21:41:44 +0300

    Design inspo working authentication working

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,45 @@
-import Sidebar from './components/sidebar';
+import { Fragment, useState } from 'react'
+import { Dialog, Menu, Transition } from '@headlessui/react'
+import {
+  Bars3Icon,
+  BellIcon,
+  CalendarIcon,
+  ChartPieIcon,
+  Cog6ToothIcon,
+  DocumentDuplicateIcon,
+  FolderIcon,
+  HomeIcon,
+  UsersIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 
-export default function RootLayout({ children }) {
+// ... [Other imports from your original layout.js file]
+
+const navigation = [
+  // ... [Your navigation items]
+]
+const teams = [
+  // ... [Your teams or other relevant data]
+]
+const userNavigation = [
+  // ... [Your user navigation items]
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Layout({ children }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
   return (
-    <div className="flex">
-      <Sidebar />
-      <main className="flex-1">
-        {children}
+    <>
+      {/* ... [The entire UI code you provided] */}
+
+      <main className="py-10">
+        <div className="px-4 sm:px-6 lg:px-8">{children}</div>
       </main>
-    </div>
-  );
+    </>
+  )
 }
