571d3e67ba5b11edd0c26c05b91dbc6f0e7302f2
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-22 10:34:32 +0300

    Design inspo working authentication working

--- app/layout.js
+++ app/layout.js
@@ -80,17 +80,19 @@ export default function Layout({ children }) {
                     {user ? (
                       <li>
                         <LogoutButton logoutDestination="https://crossfit-efesop.vercel.app/api/auth/logout">
-                          <div className="flex items-center space-x-2 p-2 hover:bg-blue-500 hover:text-white rounded">
+                          <a
+                            className="block px-3 py-1 text-sm leading-6 text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2"
+                          >
                             <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                             <span>Logout</span>
-                          </div>
+                          </a>
                         </LogoutButton>
                       </li>
                     ) : (
                       <li>
                         <a
                           href="/api/auth/login"
-                          className="flex items-center space-x-2 p-2 hover:bg-blue-500 hover:text-white rounded"
+                          className="block px-3 py-1 text-sm leading-6 text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2"
                         >
                           <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                           <span>Sign in</span>
@@ -105,92 +107,92 @@ export default function Layout({ children }) {
         </div>
 
         <div className="lg:pl-72">
-  <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-    <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => setSidebarOpen(true)}>
-      <span className="sr-only">Open sidebar</span>
-      <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-    </button>
+          <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+            <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => setSidebarOpen(true)}>
+              <span className="sr-only">Open sidebar</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
 
-    <div className="h-6 w-px bg-gray-200 lg:hidden" aria-hidden="true" />
+            <div className="h-6 w-px bg-gray-200 lg:hidden" aria-hidden="true" />
 
-    <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-      <form className="relative flex flex-1" action="#" method="GET">
-        <label htmlFor="search-field" className="sr-only">
-          Search
-        </label>
-        <MagnifyingGlassIcon
-          className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
-          aria-hidden="true"
-        />
-        <input
-          id="search-field"
-          className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-          placeholder="Search..."
-          type="search"
-          name="search"
-        />
-      </form>
-      <div className="flex items-center gap-x-4 lg:gap-x-6">
-        <button type="button" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
-          <span className="sr-only">View notifications</span>
-          <BellIcon className="h-6 w-6" aria-hidden="true" />
-        </button>
+            <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
+              <form className="relative flex flex-1" action="#" method="GET">
+                <label htmlFor="search-field" className="sr-only">
+                  Search
+                </label>
+                <MagnifyingGlassIcon
+                  className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                <input
+                  id="search-field"
+                  className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
+                  placeholder="Search..."
+                  type="search"
+                  name="search"
+                />
+              </form>
+              <div className="flex items-center gap-x-4 lg:gap-x-6">
+                <button type="button" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
 
-        {user ? (
-          // Profile dropdown for signed-in user
-          <Menu as="div" className="relative">
-            <Menu.Button className="-m-1.5 flex items-center p-1.5">
-              <span className="sr-only">Open user menu</span>
-              <img
-                className="h-8 w-8 rounded-full bg-gray-50"
-                src={user?.picture}
-                alt={user?.name}
-              />
-              <span className="hidden lg:flex lg:items-center">
-                <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
-                  {user?.name}
-                </span>
-                <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
-              </span>
-            </Menu.Button>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                <Menu.Item>
-                  <a href="/profile" className="block px-3 py-1 text-sm leading-6 text-gray-900">
-                    Your profile
+                {user ? (
+                  // Profile dropdown for signed-in user
+                  <Menu as="div" className="relative">
+                    <Menu.Button className="-m-1.5 flex items-center p-1.5">
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full bg-gray-50"
+                        src={user?.picture}
+                        alt={user?.name}
+                      />
+                      <span className="hidden lg:flex lg:items-center">
+                        <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
+                          {user?.name}
+                        </span>
+                        <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                      </span>
+                    </Menu.Button>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                        <Menu.Item>
+                          <a href="/profile" className="block px-3 py-1 text-sm leading-6 text-gray-900">
+                            Your profile
+                          </a>
+                        </Menu.Item>
+                        <Menu.Item>
+                          <a href="/api/auth/logout" className="block px-3 py-1 text-sm leading-6 text-gray-900">
+                            Sign out
+                          </a>
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                ) : (
+                  // Sign-in button for signed-out user
+                  <a href="/api/auth/login" className="block px-3 py-1 text-sm leading-6 text-gray-900 hover:bg-blue-500 hover:text-white rounded">
+                    Sign in
                   </a>
-                </Menu.Item>
-                <Menu.Item>
-                  <a href="/api/auth/logout" className="block px-3 py-1 text-sm leading-6 text-gray-900">
-                    Sign out
-                  </a>
-                </Menu.Item>
-              </Menu.Items>
-            </Transition>
-          </Menu>
-        ) : (
-          // Sign-in button for signed-out user
-          <a href="/api/auth/login" className="block px-3 py-1 text-sm leading-6 text-gray-900 hover:bg-blue-500 hover:text-white rounded">
-            Sign in
-          </a>
-        )}
+                )}
+              </div>
+            </div>
+          </div>
+
+          <main className="py-10">
+            <div className="px-4 sm:px-6 lg:px-8">{children}</div>
+          </main>
+        </div>
       </div>
-    </div>
-  </div>
-
-  <main className="py-10">
-    <div className="px-4 sm:px-6 lg:px-8">{children}</div>
-  </main>
-</div>
-    </div>
-  </>
-);
+    </>
+  );
 }
