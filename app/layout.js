443b6349386b535bee981a6e6c24774efaea0f4e
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-22 12:35:26 +0300

    Design inspo new ui

--- app/layout.js
+++ app/layout.js
@@ -43,8 +43,8 @@ export default function Layout({ children }) {
         </Transition.Root>
 
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-  <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
-    <div className="flex h-16 shrink-0 items-center">
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r bg-gray-900 px-6 pb-4">
+        <div className="flex h-16 shrink-0 items-center bg-gray-900">
       <img
         className="h-8 w-auto"
         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
@@ -55,48 +55,48 @@ export default function Layout({ children }) {
       <ul role="list" className="flex flex-1 flex-col gap-y-7">
         <li>
           <ul role="list" className="-mx-2 space-y-1">
-            {navigation.map((item) => (
-              <li key={item.name}>
-                <a
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-50 text-indigo-600'
-                      : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
-                    'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                  )}
-                >
-                  <item.icon
-                    className={classNames(
-                      item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600',
-                      'h-6 w-6 shrink-0'
-                    )}
-                    aria-hidden="true"
-                  />
-                  {item.name}
-                </a>
-              </li>
+          {navigation.map((item) => (
+  <li key={item.name}>
+    <a
+      href={item.href}
+      className={classNames(
+        item.current
+          ? 'bg-gray-800 text-white'
+          : 'text-gray-400 hover:text-white hover:bg-gray-800',
+        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+      )}
+    >
+      <item.icon
+        className={classNames(
+          item.current ? 'text-white' : 'text-gray-400 group-hover:text-white',
+          'h-6 w-6 shrink-0'
+        )}
+        aria-hidden="true"
+      />
+      {item.name}
+    </a>
+  </li>
             ))}
             {user ? (
               <li>
                 <LogoutButton logoutDestination="https://crossfit-efesop.vercel.app/api/auth/logout">
-                  <a
-                    className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                  >
-                    <XMarkIcon className="h-6 w-6 text-gray-400 group-hover:text-indigo-600" aria-hidden="true" />
-                    <span>Logout</span>
-                  </a>
-                </LogoutButton>
+  <a
+    className="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+  >
+    <XMarkIcon className="h-6 w-6 text-gray-400 group-hover:text-white" aria-hidden="true" />
+    <span>Logout</span>
+  </a>
+</LogoutButton>
               </li>
             ) : (
               <li>
                 <a
-                  href="/api/auth/login"
-                  className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                >
-                  <XMarkIcon className="h-6 w-6 text-gray-400 group-hover:text-indigo-600" aria-hidden="true" />
-                  <span>Sign in</span>
-                </a>
+  href="/api/auth/login"
+  className="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+>
+  <XMarkIcon className="h-6 w-6 text-gray-400 group-hover:text-white" aria-hidden="true" />
+  <span>Sign in</span>
+</a>
               </li>
             )}
           </ul>
