e750ed57a557617beba0553e95785893c21ff73c
Efesop <128099342+Efesop@users.noreply.github.com>
2023-08-22 10:40:16 +0300

    Design inspo new ui

--- app/layout.js
+++ app/layout.js
@@ -43,68 +43,69 @@ export default function Layout({ children }) {
         </Transition.Root>
 
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
-            <div className="flex h-16 shrink-0 items-center">
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
-              />
-            </div>
-            <nav className="flex flex-1 flex-col">
-              <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                <li>
-                  <ul role="list" className="-mx-2 space-y-1">
-                    {navigation.map((item) => (
-                      <li key={item.name}>
-                        <a
-                          href={item.href}
-                          className={classNames(
-                            item.current
-                              ? 'bg-gray-50 text-indigo-600'
-                              : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
-                            'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                          )}
-                        >
-                          <item.icon
-                            className={classNames(
-                              item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600',
-                              'h-6 w-6 shrink-0'
-                            )}
-                            aria-hidden="true"
-                          />
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                    {user ? (
-                      <li>
-                        <LogoutButton logoutDestination="https://crossfit-efesop.vercel.app/api/auth/logout">
-                          <a
-                            className="block px-3 py-1 text-sm leading-6 text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2"
-                          >
-                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                            <span>Logout</span>
-                          </a>
-                        </LogoutButton>
-                      </li>
-                    ) : (
-                      <li>
-                        <a
-                          href="/api/auth/login"
-                          className="block px-3 py-1 text-sm leading-6 text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2"
-                        >
-                          <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                          <span>Sign in</span>
-                        </a>
-                      </li>
+  <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
+    <div className="flex h-16 shrink-0 items-center">
+      <img
+        className="h-8 w-auto"
+        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+        alt="Your Company"
+      />
+    </div>
+    <nav className="flex flex-1 flex-col">
+      <ul role="list" className="flex flex-1 flex-col gap-y-7">
+        <li>
+          <ul role="list" className="-mx-2 space-y-1">
+            {navigation.map((item) => (
+              <li key={item.name}>
+                <a
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? 'bg-gray-50 text-indigo-600'
+                      : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
+                    'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                  )}
+                >
+                  <item.icon
+                    className={classNames(
+                      item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600',
+                      'h-6 w-6 shrink-0'
                     )}
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
+                    aria-hidden="true"
+                  />
+                  {item.name}
+                </a>
+              </li>
+            ))}
+            {user ? (
+              <li>
+                <LogoutButton logoutDestination="https://crossfit-efesop.vercel.app/api/auth/logout">
+                  <a
+                    className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                  >
+                    <XMarkIcon className="h-6 w-6 text-gray-400 group-hover:text-indigo-600" aria-hidden="true" />
+                    <span>Logout</span>
+                  </a>
+                </LogoutButton>
+              </li>
+            ) : (
+              <li>
+                <a
+                  href="/api/auth/login"
+                  className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                >
+                  <XMarkIcon className="h-6 w-6 text-gray-400 group-hover:text-indigo-600" aria-hidden="true" />
+                  <span>Sign in</span>
+                </a>
+              </li>
+            )}
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</div>
+
 
         <div className="lg:pl-72">
           <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
